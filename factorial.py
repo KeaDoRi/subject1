aa870e42b562ac3cfce3003009a61a183f3b9a5a
dukim <sajacall82@naver.com>
2023-09-25 23:32:46 +0900

    feat: implement Factorial class
    implement Factorial class
    implement helper function that is factorial function

--- factorial.py
+++ factorial.py
@@ -1,2 +1,21 @@
+class Factorial:
+    def __init__(self, num: int):
+        """
+        num: int, target for factorial
+        """
+        self._target = num
+    
+    def __call__(self):
+        return self.play()
+    
+    def play(self):
+        if self._target == 1:
+            return 1
+        else:
+            return self._target * factorial(self._target-1)
+
+def factorial(num):
+    return Factorial(num)()
+
 if __name__=='__main__':
-    print('factorial')
+    print(factorial(11))
