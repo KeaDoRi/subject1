3c7c39cb576b5538702a583d44bae041238c93df
JJJJiwon <jwjw0752@gmail.com>
2023-09-27 21:10:26 +0900

    feat: create factorial.py

--- factorial.py
+++ factorial.py
@@ -1,1 +1,6 @@
-print("영진님........... 정식으로 항의하겠습니다... 초보자를 위해 쉬운 문제도 내주세효...! 굽신")
+# for문을 사용한 팩토리얼 소스 코드
+a = int(input("팩토리얼을 구할 숫자를 입력하세요 : "))
+result = 1
+for item in range(1, a+1, 1):
+    result *= item      #result = result * item
+print(result)
