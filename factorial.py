9fbae15cd4da549477d23273f8e59a7a34b37491
eunzzae <ebrhda@gmail.com>
2023-09-28 22:59:51 +0900

    fead : Add __init__ function, print num

--- factorial.py
+++ factorial.py
@@ -1,4 +1,11 @@
 class factorial : 
-    pass
+    def __init__(self, num):
+        self.num = num
 
-f = factorial()
+    def func_num1(self):
+        print(self.num)
+    
+    def func_num2(self):
+        return self.num
+
+f = factorial(3)
