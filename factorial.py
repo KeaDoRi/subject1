b1389507fab1b56990d9c82b7d6d63cff62553b2
JaeminKIM-Irene <kjmkjk97@daum.net>
2023-09-29 18:16:47 +0900

    feat: Add contents to __init__ and start function
    Start function print out the result of factorial(num).

--- factorial.py
+++ factorial.py
@@ -1,5 +1,23 @@
 class Factorial :
-    def __init__(self, num) :
-        
-    def start(self, num) :
+    def __init__(self, num: int) :
+        """
+        Initialize target number with the number given by user.
 
+        Attributes :
+            num (int): target number to do factorial
+        """
+        self.num = num
+
+    def start(self) :
+        """
+        Print out the result of factorial(num).
+        Factorial is a process of multiplying numbers from 1 to num sequentially.
+        """
+        result = 1
+        for i in range(2, self.num+1) :
+            result *= i
+        print(result)
+    
+if __name__ == '__main__' :
+    fact = Factorial(1)
+    fact.start()
